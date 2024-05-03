72c9f99305c6a82f630096674610b94ea583f048
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2024-05-03 20:12:11 +0200

    Pass the Xorg socket to the container

--- run.sh
+++ run.sh
@@ -5,9 +5,23 @@ if ! [ -d "src" ]; then
 	exit 1
 fi
 
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch "${XAUTH}"
+xauth nlist "${DISPLAY}" | sed -e 's/^..../ffff/' | xauth -f "${XAUTH}" nmerge -
+chmod 644 "${XAUTH}"
+
 docker run \
   -it \
-  --volume "${PWD}"/src:/root/ws/src \
+  --env=DISPLAY="${DISPLAY}"  \
+  --env=QT_X11_NO_MITSHM=1 \
+  --env=XDG_RUNTIME_DIR=/tmp \
+  --env=WAYLAND_DISPLAY="${WAYLAND_DISPLAY}" \
+  --volume="${XDG_RUNTIME_DIR}"/"${WAYLAND_DISPLAY}":/tmp/"${WAYLAND_DISPLAY}" \
+  --env=XAUTHORITY="${XAUTH}" \
+  --volume="${XAUTH}":"${XAUTH}" \
+  --volume="${XSOCK}":"${XSOCK}" \
+  --volume ./src:/root/ws/src \
   --name=ros_robot_demo \
   ros_robot_demo \
   bash
